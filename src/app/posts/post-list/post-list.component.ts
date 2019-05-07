ac6320b4c2a5ddefd6d00760fb8fb58c3895cc96
StyleMaxz <stylemaxz@gmail.com>
2019-05-07 22:56:33 +0700

    add file post-list

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  // posts = [
+  //   {title: 'First Post', content: 'This is the first post content'},
+  //   {title: 'Second Post', content: 'This is the second post content'},
+  //   {title: 'Thrid Post', content: 'This is the third post content'},
+  // ];
+
+  @Input() posts = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
